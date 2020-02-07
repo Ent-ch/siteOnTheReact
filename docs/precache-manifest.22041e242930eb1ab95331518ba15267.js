967610194176ef969b409ccc33efb6a348b8326a
syst3mat1c <runrajn@gmail.com>
2020-02-07 13:06:51 +0300

    add docs folder for git pages

---
+++ docs/precache-manifest.22041e242930eb1ab95331518ba15267.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36215cee4d070121c187d0594ccb7364",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f501fab4f4f80634c191",
+    "url": "/static/css/main.5a32e041.chunk.css"
+  },
+  {
+    "revision": "a22729a4253d3c1abc33",
+    "url": "/static/js/2.08ac724c.chunk.js"
+  },
+  {
+    "revision": "05182bf3097604151dd2f67e79167c1a",
+    "url": "/static/js/2.08ac724c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f501fab4f4f80634c191",
+    "url": "/static/js/main.daa9133f.chunk.js"
+  },
+  {
+    "revision": "9af90a3bdb44f1b1746a",
+    "url": "/static/js/runtime-main.fb18d836.js"
+  },
+  {
+    "revision": "d98240c5e23ad4326274d6ff1392ce22",
+    "url": "/static/media/beach.d98240c5.png"
+  },
+  {
+    "revision": "384832493824d3319a763ee0cb8134de",
+    "url": "/static/media/beach2.38483249.png"
+  },
+  {
+    "revision": "09829dada255c8fc555c92826ca49bd1",
+    "url": "/static/media/beach3.09829dad.png"
+  },
+  {
+    "revision": "35999d7137a3724d29bfaa372fa2ef70",
+    "url": "/static/media/bs_logo_fff.35999d71.png"
+  },
+  {
+    "revision": "488208fa4ca84df7ad322e1fdebfa005",
+    "url": "/static/media/filter-kidsfri.488208fa.png"
+  },
+  {
+    "revision": "c216761ba43db6e6c8a58f452a3a8ed4",
+    "url": "/static/media/filter-relax.c216761b.png"
+  },
+  {
+    "revision": "3afadc041ff37143e09c98f8c8a131f5",
+    "url": "/static/media/filter-sandwater.3afadc04.png"
+  },
+  {
+    "revision": "ca035e2b3ac70207d33fedb08582960a",
+    "url": "/static/media/filter-shadow.ca035e2b.png"
+  },
+  {
+    "revision": "ea60316f1a7757340a8e8551ff170575",
+    "url": "/static/media/filter-sports.ea60316f.png"
+  },
+  {
+    "revision": "c6554d6ded508242d567259e61fdaab7",
+    "url": "/static/media/header_back.c6554d6d.png"
+  },
+  {
+    "revision": "11b80450edc6e244121c56050e2ce836",
+    "url": "/static/media/icon-arrow-down.11b80450.svg"
+  },
+  {
+    "revision": "06218cd5613da70023c2f0a6d31d775e",
+    "url": "/static/media/icon-arrow-right.06218cd5.svg"
+  },
+  {
+    "revision": "ab0127bcc7e8351a05b8cc027aabb498",
+    "url": "/static/media/icon-camera.ab0127bc.svg"
+  },
+  {
+    "revision": "88f7e7401d27aa154efad8576bbba3c8",
+    "url": "/static/media/icon-lang.88f7e740.svg"
+  },
+  {
+    "revision": "46774ca7735ebaa00ff75da8e24576c4",
+    "url": "/static/media/icon-mark.46774ca7.svg"
+  },
+  {
+    "revision": "c1a7bff0482a8755377a507fa431d51f",
+    "url": "/static/media/icon-pen-blue.c1a7bff0.svg"
+  },
+  {
+    "revision": "1a954ca6dc9884ed3e4e53cabaa30414",
+    "url": "/static/media/icon-pen.1a954ca6.svg"
+  },
+  {
+    "revision": "5eae86822eb38ae97be8bbe94e9d2d8e",
+    "url": "/static/media/icon-user.5eae8682.svg"
+  },
+  {
+    "revision": "16fda4630a5039069c365f1b88aa269b",
+    "url": "/static/media/region-lefkas.16fda463.png"
+  },
+  {
+    "revision": "1a74e9f82cefaab7e16f89ed8b955913",
+    "url": "/static/media/region-mallorca.1a74e9f8.png"
+  },
+  {
+    "revision": "8b7ac260f17ee4629f6edd32652fe613",
+    "url": "/static/media/region-menorca.8b7ac260.png"
+  }
+]);
